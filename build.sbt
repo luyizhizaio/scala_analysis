de34f4e1abc05374afb149e4100104dfa795b196
findaway <lichangyue528@sina.com>
2017-05-25 23:42:38 +0800

    init test

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "scala_analysis"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+    
---
+++ src/main/scala/com/kyrie/test/Test1.scala
@@ -0,0 +1,13 @@
+package com.kyrie.test
+
+/**
+ * Created by dayue on 2017/5/25.
+ */
+object Test1 {
+
+
+  def main(args: Array[String]) {
+    println("fuck scala")
+  }
+
+}
