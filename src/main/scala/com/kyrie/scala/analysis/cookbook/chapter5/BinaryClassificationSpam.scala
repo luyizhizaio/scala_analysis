de877ad5a850a731c9f1ac0411dbb1ad7fb91f93
changyue.li <changyue.li@tendcloud.com>
2017-06-01 11:26:02 +0800

    抄袭

--- src/main/scala/com/kyrie/scala/analysis/cookbook/chapter5/BinaryClassificationSpam.scala
+++ src/main/scala/com/kyrie/scala/analysis/cookbook/chapter5/BinaryClassificationSpam.scala
@@ -1,11 +1,36 @@
 package com.kyrie.scala.analysis.cookbook.chapter5
 
 import java.util.Properties
-
+import scala.collection.JavaConverters.asScalaBufferConverter
+import scala.io.Source
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.classification.LogisticRegressionWithSGD
+import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
-import org.apache.spark.{SparkContext, SparkConf}
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation
+import edu.stanford.nlp.pipeline.Annotation
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.optimization.Updater
+import org.apache.spark.mllib.optimization.L1Updater
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.regression.GeneralizedLinearModel
+import org.apache.spark.mllib.regression.GeneralizedLinearAlgorithm
+import org.apache.spark.mllib.optimization.SquaredL2Updater
+import epic.preprocess.TreebankTokenizer
+import epic.preprocess.MLSentenceSegmenter
+import org.apache.spark.mllib.feature.IDF
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.feature.Normalizer
+import org.apache.spark.mllib.feature.IDFModel
+import org.apache.spark.ml.feature.Tokenizer
 
 /**
  * Created by dayue on 2017/5/29.
@@ -26,10 +51,84 @@ object BinaryClassificationSpam extends App {
   }
 
   val labeledPointsUsingStanfordNLPRdd = getLabeledPoints(docs,"STANFORD")
+  val lpTfIdf=withIdf(labeledPointsUsingStanfordNLPRdd).cache()
+
+  //Split dataset
+  val spamPoints = lpTfIdf.filter(point => point.label == 1).randomSplit(Array(0.8, 0.2))
+  val hamPoints = lpTfIdf.filter(point => point.label == 0).randomSplit(Array(0.8, 0.2))
+
+  println("Spam count:" + (spamPoints(0).count) + "::" + (spamPoints(1).count))
+  println("Ham count:" + (hamPoints(0).count) + "::" + (hamPoints(1).count))
+
+  val trainingSpamSplit = spamPoints(0)
+  val testSpamSplit = spamPoints(1)
+
+  val trainingHamSplit = hamPoints(0)
+  val testHamSplit = hamPoints(1)
+
+  val trainingSplit = trainingSpamSplit ++ trainingHamSplit
+  val testSplit = testSpamSplit ++ testHamSplit
+
+
+  val logisticWithSGD = getAlgorithm("LOGSGD", 100, 1, 0.001)
+  val logisticWithBfgs = getAlgorithm("LOGBFGS", 100, 1, 0.001)
+  val svmWithSGD = getAlgorithm("SVMSGD", 100, 1, 0.001)
 
 
 
 
+
+  def runClassification(algorithm :GeneralizedLinearAlgorithm[_ <:GeneralizedLinearModel],
+                         trainData:RDD[LabeledPoint],testData:RDD[LabeledPoint]):RDD[(Double,Double)]={
+
+    val mode = algorithm.run(trainData)
+    val predicted = model.predict(testData.map(point => point.features))
+    val actuals = testData.map(point => point.label)
+    val predictAndActuals:RDD[(Double,Double)] = predicted.zip(actuals)
+
+    predictAndActuals
+
+  }
+
+
+
+  def gteAlgorithn(algo:String,iterations:Int, stepSize:Double,regParam:Double) = algo match{
+
+    case "LOGSGD" => {
+      val algo = new LogisticRegressionWithSGD()
+      algo.setIntercept(true).optimizer.setNumIterations(iterations).setStepSize(stepSize).setRegParam(regParam)
+      algo
+    }
+    case "LOGBFGS" => {
+      val algo = new LogisticRegressionWithLBFGS()
+      algo.setIntercept(true).optimizer.setNumIterations(iterations).setRegParam(regParam)
+      algo
+    }
+    case "SVMSGD" => {
+      val algo = new SVMWithSGD()
+      algo.setIntercept(true).optimizer.setNumIterations(iterations).setStepSize(stepSize).setRegParam(regParam)
+      algo
+    }
+
+  }
+
+
+
+  def withIdf(lPoints: RDD[LabeledPoint]): RDD[LabeledPoint] = {
+    val hashedFeatures = lPoints.map(lp => lp.features)
+    val idf: IDF = new IDF()
+    val idfModel: IDFModel = idf.fit(hashedFeatures)
+
+    val tfIdf: RDD[Vector] = idfModel.transform(hashedFeatures)
+
+    val lpTfIdf= lPoints.zip(tfIdf).map {
+      case (originalLPoint, tfIdfVector) => {
+        new LabeledPoint(originalLPoint.label, tfIdfVector)
+      }
+    }
+
+    lpTfIdf
+  }
 
 
   def getLabeledPoints(docs: RDD[Document], library: String): RDD[LabeledPoint] = library match {
